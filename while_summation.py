ff7ba1fd6639f9c8f824175b0fbcf6b19f690e29
BridgerCozzens <rasungod065@gmail.com>
2025-09-24 10:03:41 -0600

    Update while_summation.py

--- while_summation.py
+++ while_summation.py
@@ -1,1 +1,10 @@
-#while summation code following directions from Readme
+input = int(input("   Put the number you would like to sum up to     "))
+
+x = 0
+
+i=1
+
+while i <= input:
+    x += i
+    i += 1
+print (x)
